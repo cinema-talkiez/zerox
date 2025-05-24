0eae9d6ff254828311057114c39b1398b04037a3
cinema-talkiez <cinema.talkiez.io@gmail.com>
2025-05-25 01:08:55 +0530

    Update index1.js

--- pages/index1.js
+++ pages/index1.js
@@ -137,7 +137,7 @@ const Index1 = () => {
           </div>
 <div
             style={cardStyle}
-            onClick={() => handleRedirect("https://esaibomma.link/")}
+            onClick={() => handleRedirect("https://google.com/")}
             onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.03)")}
             onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
           >
