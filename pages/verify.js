ddab7046468cfca4c27557d274db99e29300ddad
cinema-talkiez <cinema.talkiez.io@gmail.com>
2025-07-09 18:15:27 +0530

    Update verify.js

--- pages/verify.js
+++ pages/verify.js
@@ -11,7 +11,7 @@ export default function VerifyPage() {
     setErrorMessage("");
 
     try {
-      const fixedUrl = "https://exe.io/YM7Bi";
+      const fixedUrl = "https://sfl.gl/N6nc";
       window.location.href = fixedUrl; // Works in WebView and browser
     } catch (error) {
       setErrorMessage("An error occurred while opening the verification link.");
